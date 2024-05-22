33f2a7c8e3d841c08d109aa765f9d07d2aab46b2
DOBI <70043120+wnsgml517@users.noreply.github.com>
2024-05-22 23:51:58 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,20 +1,33 @@
 N = int(input())
 
+maxi = 0
+maxj = 0
+
+li = []
+
 for _ in range (N):
     i = int(input())
     j = int(input())
 
-    arr = [[0 for _ in range(j)] for _ in range(i+1)]
+    li.append((i,j))
+    
+    maxi = max(i,maxi)
+    maxj = max(j, maxj)
+    
+for _ in range (N):
+
+    arr = [[0 for _ in range(maxj)] for _ in range(maxi+1)]
 
 
-    for index in range(j):
+    for index in range(maxj):
         arr[0][index] = index+1
     
     
     
-    for floor in range(1, i+1):
-        for ho in range(j):
-            for value in range(ho+1):                
-                arr[floor][ho]+=arr[floor-1][value]
-           
-    print(arr[i][j-1], end =" ")
+    for floor in range(1, maxi+1):
+        for ho in range(maxj):
+            arr[floor][ho] = arr[floor-1][ho]+arr[floor][ho-1]
+
+
+for i in li:
+    print(arr[i[0]][i[1]-1], end =" ")
