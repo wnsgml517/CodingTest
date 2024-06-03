b184a0e0b2884df1a2a32d91ae286dc76147e9de
DOBI <70043120+wnsgml517@users.noreply.github.com>
2024-06-03 23:19:16 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,37 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+stack = []
+mode = {"pop": 0 ,"size":1,"empty":2,"top":3,"push":4}
+
+for i in range (N):
+    li = sys.stdin.readline().split()
+    n = len(stack)
+    
+    index = mode[li[0]]
+    
+    if index == 0:
+        if n==0:
+            print(-1)
+        else:
+            print(stack.pop())
+            
+    elif index == 1:
+        print(len(stack))
+
+    elif index == 2:
+        if n==0:
+            print(1)
+        else:
+            print(0)
+
+    elif index == 3:
+        if n == 0:
+            print(-1)
+        else:
+            print(stack[-1])
+
+    elif index == 4:
+        stack.append(li[1])
+
