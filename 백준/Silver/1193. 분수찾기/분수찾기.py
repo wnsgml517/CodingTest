ef3f61d8221c06e8837ea3533b63ef4d18be8673
DOBI <70043120+wnsgml517@users.noreply.github.com>
2024-06-04 21:38:59 +0900

    [Silver V] Title: 분수찾기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -1,12 +1,22 @@
-a = int(input())
-i = 0
-while a >0:
-    a -= i
-    i += 1
-    
-a = i+a-1
-res = str(a)+'/'+str(i-a)
-if i %2 == 0:
-    res = str(i-a)+'/'+str(a)
+import sys
 
-print(res)
+N = int(sys.stdin.readline())
+
+cnt = 0
+edge = 1
+i = 1
+
+while (True): # 1부터 N까지 변화...
+    cnt+=i
+    if cnt>=N:
+        break
+    
+    i+=1
+
+start = i+N-cnt
+end = i- start+1
+
+if i %2 == 0:
+    print(f"{start}/{end}")
+else:
+    print(f"{end}/{start}")
